278de8aecc95969f70366ef4314d105349c32d68
Grigory Sadovskiy <sadovskiy.grigory@yandex.ru>
2021-06-03 17:36:02 +0300

    Raw js content

---
+++ script.js
@@ -0,0 +1,42 @@
+
+let playerScore = 0;
+let computerScore = 0;
+
+const emojiButtons = document.querySelectorAll('.emoji');
+// emojiButtons.forEach((button) => button.addEventListener('click', func));
+
+console.log(emojiButtons[0].value)
+
+// gameChoices.addEventListener('click', event => {button. = `Click count: ${event.detail}`;
+// });
+
+// rockChoice.addEventListener("click", function() {
+//     let increment = document.getElementById("#count");
+//     increment.value = count;
+//     count++;
+//   });
+
+
+// function randomComputer(){
+//   let options = ['Rock', 'Paper', 'Scissors']
+//   return (options[Math.floor(Math.random() * options.length)])
+// }
+
+
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+//         console.log(`Player has ${playerSelection.toLowerCase()} and Computer has ${computerSelection.toLowerCase()}. It's a tie!`)
+//     } else if (computerSelection.toLowerCase() === )
+//   }
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+
+
+// function game() {
+//     for (let counter = 0; counter < 5; counter++) {
+
+//     }
+
+// }
